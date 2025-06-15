1063268707fe4ec1213d86e9c26d8abafda9cebd
RaihanShakeel <raihanshakeel58@gmail.com>
2025-06-15 16:43:36 +0500

    Built issue details page

---
+++ app/issues/[id]/page.tsx
@@ -0,0 +1,33 @@
+import IssueStatusBadge from '@/app/components/IssueStatusBasdge'
+import { prisma } from '@/prisma/client'
+import { Card, Flex, Heading , Text} from '@radix-ui/themes'
+import { notFound } from 'next/navigation'
+import React from 'react'
+
+interface Props{
+    params: Promise<{id: string}>;
+}
+
+export default async function  ({params}: Props) {
+    const resolveParams = await params;
+    const issue = await prisma.issue.findUnique({
+        where: {
+            id: parseInt(resolveParams.id)
+        }
+    })
+
+    if (!issue) notFound();
+
+    return (
+    <div>
+        <Heading>{issue.title}</Heading>
+        <Flex className='space-x-3' my={'3'}>
+            <IssueStatusBadge status={issue.status}/>
+            <Text>{issue.createdAt.toDateString()}</Text>
+        </Flex>
+        <Card>
+            <Text>{issue.description}</Text>
+        </Card>
+    </div>
+  )
+}
