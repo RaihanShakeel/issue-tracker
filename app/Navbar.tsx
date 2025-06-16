9ac53a0c6e396d04e6d0d0994fdef9e7d81edc6d
RaihanShakeel <raihanshakeel58@gmail.com>
2025-06-16 09:21:02 +0500

    Added markdown preview

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'>
         <ImBug href='/'/>
-        <ul className='flex space-x-6'>
+        <ul className='flex space-x-6 decoration-0'>
             {links.map((link) => <Link className={classnames({'text-zinc-500': currentPath !== link.href, 'text-zinc-900': currentPath === link.href, 'hover:text-zinc-800 transition-colors': true})}  key={link.href} href={link.href}>{link.label}</Link>)}
         </ul>
     </nav>
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -3,6 +3,7 @@ import { prisma } from '@/prisma/client'
 import { Card, Flex, Heading , Text} from '@radix-ui/themes'
 import { notFound } from 'next/navigation'
 import React from 'react'
+import ReactMarkDown from 'react-markdown';
 
 interface Props{
     params: Promise<{id: string}>;
@@ -25,8 +26,8 @@ export default async function  ({params}: Props) {
             <IssueStatusBadge status={issue.status}/>
             <Text>{issue.createdAt.toDateString()}</Text>
         </Flex>
-        <Card>
-            <Text>{issue.description}</Text>
+        <Card className='!prose !lg:prose-xl'>
+            <ReactMarkDown>{issue.description}</ReactMarkDown>
         </Card>
     </div>
   )
