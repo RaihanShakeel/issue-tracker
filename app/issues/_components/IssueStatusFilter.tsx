86f55d20856e9b54e7ee6c810a4a22ccabdd9af6
RaihanShakeel <raihanshakeel58@gmail.com>
2025-06-21 08:20:16 +0500

    Built the IssueStatusFilter component

---
+++ app/issues/_components/IssueStatusFilter.tsx
@@ -0,0 +1,26 @@
+import { Issue, Status } from '@/app/generated/prisma'
+import { DropdownMenu, Select } from '@radix-ui/themes'
+import React from 'react'
+
+
+const statuses: {label: string, value?: Status} [] = [
+    {label: 'All'},
+    {label: 'Open',  value: "OPEN"},
+    {label: "In-progress", value: 'IN_PROGRESS'},
+    {label: "Closed", value: 'CLOSED'}
+]
+
+const IssueStatusFilter = () => {
+  return (
+    <Select.Root>
+        <Select.Trigger placeholder='Filter Issue by status...'/>
+        <Select.Content>
+            {statuses.map(status => (
+                <Select.Item value={status.value || 'All'} key={status.label}>{status.label}</Select.Item>
+            ))}
+        </Select.Content>
+    </Select.Root>
+  )
+}
+
+export default IssueStatusFilter
